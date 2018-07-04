bcbcbd1318400b1fec42a77f7a0d8992dc289d78
os0022 <oaaos@hotmail.com>
2018-07-04 17:19:01 +0800

    install Clearance gem for user management

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'sign_up/index'
+
+  root 'sign_up#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
