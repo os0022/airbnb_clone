f640c6dfcc2074c84fb25074d39b19ee19c97d85
os0022 <oaaos@hotmail.com>
2018-07-18 15:17:36 +0800

    harmony

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+
+  config.assets.js_compressor = Uglifier.new(
+    # ES6 support
+    :harmony => true
+)
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
